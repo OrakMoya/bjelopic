cd670603c6f00a102c2be97898bbe2619ed6dd6f
Mate Pušić <orakmoyaamitsira@gmail.com>
2023-11-30 23:52:35 +0100

    Landing page design

---
+++ src/routes/(homepage)/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Header from "../Header.svelte";
+    import Footer from "../Footer.svelte";
+    import { fly } from 'svelte/transition';
+    import { quintOut } from 'svelte/easing';
+
+    /**
+	 * @type {number}
+	 */
+    let y;
+
+    /**
+	 * @type {number}
+	 */
+    let viewport_height;
+
+    $: console.log(viewport_height);
+</script>
+
+{#if y>0}
+    <div transition:fly={{ delay: 0, duration: 700, x: 0, y: -200, opacity: 1, easing: quintOut }} class="fixed top-0 w-full z-10" >
+        <Header caption_additional_classes="{viewport_height > 700 ? ' ' : 'hidden md:block'}"/>
+    </div>
+{/if}
+<slot />
+<Footer />
+
+<svelte:window bind:scrollY={y} bind:innerHeight={viewport_height}/>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,41 @@
+<script>
+	const logo = "/logo.png"
+	const name = "BjeloPIC"
+	const caption = "Bjelovarski filmski studio"
+
+	const infoLinks = [
+		{id:1, page:"/about", name:"O nama"},
+		{id:2, page:"/team", name:"Naši zaposlenici"},
+		{id:3, page:"/history", name:"Povijest"},
+		{id:4, page:"/contact", name:"Kontakt"},
+	]
+	const socialsLinks = [
+		{id:1, page:"https://www.facebook.com/bjelopic", name:"Facebook"},
+		{id:2, page:"https://www.instagram.com/bjelopic/", name:"Instagram"},
+	]
+	const navigationSections = [
+		{id:1, section:"Informacije", links:infoLinks},
+		{id:2, section:"Društvene mreže", links:socialsLinks},
+	]
+</script>
+
+<footer class="bg-black text-white py-10">
+	<div class="flex flex-wrap justify-between max-w-screen-2xl mx-auto my-4">
+		<div>
+			<img class="w-8" src="{logo}" alt="">
+			<span class="text-2xl font-bold">{@html name}</span>
+			<p>{caption}</p>
+		</div>
+		<div class="flex justify-between my-4">
+			{#each navigationSections as linkSection}
+				<nav class="w-40 min-w-fit">
+					<p class="font-bold text-xl">{linkSection.section}</p>
+					{#each linkSection.links as link}
+						<a class="block transition-all hover:font-bold" href="{link.page}">{link.name}</a>
+					{/each}
+				</nav>
+			{/each}
+		</div>
+		<span class="w-full text-center font-bold basis-full my-4">Copyright BjeloPIC 2023 | Sva prava pridržana</span>
+	</div>
+</footer>
