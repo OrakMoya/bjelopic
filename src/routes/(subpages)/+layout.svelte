9db385233f3c5d276aef869fd2e5d42a8fa9d867
Mate Pušić <orakmoyaamitsira@gmail.com>
2023-12-01 00:10:10 +0100

    Add layout to pages other than landing

--- src/routes/(subpages)/+layout.svelte
+++ src/routes/(subpages)/+layout.svelte
@@ -1,4 +1,11 @@
 <script>
+	import Footer from '../Footer.svelte';
+import Header from '../Header.svelte';
+
     /** @type {import('./$types').LayoutData} */
     export let data;
 </script>
+
+<Header />
+<slot />
+<Footer />
