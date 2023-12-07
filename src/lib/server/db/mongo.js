60c03c6643c1e0a351632d3b8e027456a0b40519
Mate Pušić <orakmoyaamitsira@gmail.com>
2023-12-07 10:41:41 +0100

    added mongo user and password

--- src/lib/server/db/mongo.js
+++ src/lib/server/db/mongo.js
@@ -3,11 +3,15 @@ import { MongoClient } from "mongodb";
 const MONGO_URL = env.MONGO_URL;
 const MONGO_PORT = env.MONGO_PORT;
 const MONGO_DB = env.MONGO_DB;
+const MONGO_USER = env.MONGO_USER;
+const MONGO_USER_PWD = env.MONGO_USER_PWD;
 
-const client = new MongoClient("mongodb://"+MONGO_URL+":"+MONGO_PORT+"/"+MONGO_DB);
+const MONGO_CONNECTION_URL="mongodb://"+MONGO_USER+":"+MONGO_USER_PWD+"@"+MONGO_URL+":"+MONGO_PORT+"/"+MONGO_DB
+
+const client = new MongoClient(MONGO_CONNECTION_URL);
 
 export async function start_mongo() {
-	console.log("Connecting to: "+"mongodb://"+MONGO_URL+":"+MONGO_PORT+"/"+MONGO_DB)
+	console.log("Connecting with: " + MONGO_CONNECTION_URL);
 	return client.connect();
 }
 
