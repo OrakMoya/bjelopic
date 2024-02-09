f0f5488ed7c013633485a68a3d52427b74941bd5
Mate Pušić <orakmoyaamitsira@gmail.com>
2024-02-09 10:51:43 +0100

    move large files out of repo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,10 @@
 <svelte:head>
 	<title>{$website_title}</title>
 	<link rel="icon" type="image/png" href="/favicon.png" />
-	<script id="iframe_api" src="https://www.youtube.com/iframe_api"></script>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100..900&display=swap" rel="stylesheet" />
+	<script id="iframe_api" src="https://www.youtube.com/iframe_api"></script>
 </svelte:head>
 <main class="min-h-screen bg-black">
 	<slot />
