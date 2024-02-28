6a33f9a15fd5644e721e3cba41fb65a7c180878f
Mate Pušić <orakmoyaamitsira@gmail.com>
2024-02-29 00:23:56 +0100

    remove blog, make blog redirect to ghost app

--- src/routes/(homepage)/LandingScreen.svelte
+++ src/routes/(homepage)/LandingScreen.svelte
@@ -36,7 +36,7 @@
 	const subtitle_links = [
 		{ id: 1, page: '/gallery', name: 'Galerija Radova' },
 		{ id: 2, page: 'https://www.youtube.com/@bjelopic', name: 'YouTube Kanal' },
-		{id: 3, page: '/blog', name: 'Blog'},
+		{id: 3, page: 'https://blog.bjelopic.com', name: 'Blog'},
 		{ id: 4, page: '/about', name: 'O Nama' }
 	];
 </script>
